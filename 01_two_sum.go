f537a1ec088c7ddd4907c910954c0f2ad3885221
luxueyan <luxueyan@truxing.com>
2018-07-30 13:06:27 +0800

    add two sum

---
+++ 01_two_sum.go
@@ -0,0 +1,13 @@
+package leetcode
+
+//https://leetcode-cn.com/problems/two-sum/description/
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
