96312aff4d40e8598e4e66ced0d21ddeedf3569f
Anderson Lu <306887931@qq.com>
2019-02-21 09:43:59 +0800

    Create 20_valid_parentheses.go

---
+++ 20_valid_parentheses.go
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=golang
+ *
+ * [20] 有效的括号
+ *
+ * https://leetcode-cn.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (36.21%)
+ * Total Accepted:    43.9K
+ * Total Submissions: 121.3K
+ * Testcase Example:  '"()"'
+ *
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ *
+ *
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: true
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: "()[]{}"
+ * 输出: true
+ *
+ *
+ * 示例 3:
+ *
+ * 输入: "(]"
+ * 输出: false
+ *
+ *
+ * 示例 4:
+ *
+ * 输入: "([)]"
+ * 输出: false
+ *
+ *
+ * 示例 5:
+ *
+ * 输入: "{[]}"
+ * 输出: true
+ *
+ */
+func isValid(s string) bool {
+	stack := make([]rune, len(s))
+	x := 0
+	for _, ch := range s {
+		if ch == '{' || ch == '[' || ch == '(' {
+			stack[x] = ch
+			x++
+		} else {
+			if x < 1 {
+				return false
+			}
+			l := stack[x-1]
+			switch ch {
+			case '}':
+				if l != '{' {
+					return false
+				}
+			case ']':
+				if l != '[' {
+					return false
+				}
+			case ')':
+				if l != '(' {
+					return false
+				}
+			}
+			x--
+		}
+	}
+	if x != 0 {
+		return false
+	}
+	return true
+}
